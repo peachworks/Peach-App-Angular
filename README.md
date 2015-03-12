381d822cbb7326a7721dc8fa93c70ae4b4cec6dc
Jason Smylnycky <jsmylnycky@peachworks.com>
2015-03-12 09:29:05 -0400

    Initial build using a modified version of generator-gulp-angular. This build replaces Traceur with Babel, but uses Angular 1.3. All unit tests pass.  E2E tests have a glitch due to the missing .yo-rc file. Directory structure is WRONG.

--- README.md
+++ README.md
@@ -6,8 +6,13 @@ Base project for developer apps.
 Technology stack:
 
 NPM & Bower
+
 Gulp
+
 Karma Testing
+
 Angular 1.4
+
 ngNewRouter
+
 ES6
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,58 @@
+'use strict';
+/*jshint esnext: true */
+
+class MainCtrl {
+  constructor ($scope) {
+    $scope.awesomeThings = [
+      {
+        'title': 'AngularJS',
+        'url': 'https://angularjs.org/',
+        'description': 'HTML enhanced for web apps!',
+        'logo': 'angular.png'
+      },
+      {
+        'title': 'BrowserSync',
+        'url': 'http://browsersync.io/',
+        'description': 'Time-saving synchronised browser testing.',
+        'logo': 'browsersync.png'
+      },
+      {
+        'title': 'GulpJS',
+        'url': 'http://gulpjs.com/',
+        'description': 'The streaming build system.',
+        'logo': 'gulp.png'
+      },
+      {
+        'title': 'Jasmine',
+        'url': 'http://jasmine.github.io/',
+        'description': 'Behavior-Driven JavaScript.',
+        'logo': 'jasmine.png'
+      },
+      {
+        'title': 'Karma',
+        'url': 'http://karma-runner.github.io/',
+        'description': 'Spectacular Test Runner for JavaScript.',
+        'logo': 'karma.png'
+      },
+      {
+        'title': 'Protractor',
+        'url': 'https://github.com/angular/protractor',
+        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
+        'logo': 'protractor.png'
+      },
+      {
+        'title': 'ES6 (Babel)',
+        'url': 'https://github.com/babel/babel',
+        'description': 'A JavaScript.next-to-JavaScript-of-today compiler that allows you to use features from the future today.',
+        'logo': 'babel.png'
+      }
+    ];
+    $scope.awesomeThings.forEach(function(awesomeThing) {
+      awesomeThing.rank = Math.random();
+    });
+  }
+}
+
+MainCtrl.$inject = ['$scope'];
+
+export default MainCtrl;
